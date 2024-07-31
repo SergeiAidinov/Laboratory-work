797cc44b3494320c6ec2869f1776da8e858a165c
Sergei Aidinov <incoming34@yandex.ru>
2024-07-31 17:24:27 +0300

    Cleaned code

--- src/main/java/ru/yandex/incoming34/passengers_and_tickets/entity/PassengerBrief.java
+++ src/main/java/ru/yandex/incoming34/passengers_and_tickets/entity/PassengerBrief.java
@@ -1,10 +1,6 @@
 package ru.yandex.incoming34.passengers_and_tickets.entity;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.List;
 
 @Entity
 @Table(name = "passenger")
--- src/main/java/ru/yandex/incoming34/passengers_and_tickets/entity/TicketBrief.java
+++ src/main/java/ru/yandex/incoming34/passengers_and_tickets/entity/TicketBrief.java
@@ -1,7 +1,5 @@
 package ru.yandex.incoming34.passengers_and_tickets.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
